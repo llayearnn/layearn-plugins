c517d6b9efa0d620c494a3de1c90730ad2dcce80
王昌 <1255272899@qq.com>
2023-08-10 13:43:43 +0800

    first commit

---
+++ src/packages/button/index.js
@@ -0,0 +1,7 @@
+import LayearnButton from './src/LayearnButton.vue'
+export default{
+  install:(Vue)=>{
+    Vue.component('ly-button', LayearnButton);
+
+  }
+}
---
+++ src/packages/button/src/LayearnButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    layearn的插件
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LayearnButton'
+  };
+</script>
